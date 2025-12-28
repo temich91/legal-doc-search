78abd17e0f94e92620526713c432b095eba51819
temich91 <temich-ivanoff@yandex.ru>
2025-12-28 22:37:37 +0500

    quadrant client class; dummy console interface

--- src/main.py
+++ src/main.py
@@ -1,59 +1,62 @@
-import os
-import pandas as pd
-import numpy as np
-import faiss
 from src.embedder.rulaw_embedder import RuLawEmbedder
-import warnings
+from qdrant_client import QdrantClient
 
-warnings.filterwarnings(
-    "ignore",
-    message="Some weights of the model checkpoint.*"
-)
 
-def create_faiss_index(parquet_path="../data/processed", index_path="../tmp"):
-    all_embeddings = []
-    metadata = []
-    for file in os.listdir(parquet_path):
-        df = pd.read_parquet(os.path.join(parquet_path, file))
-        embeddings = np.stack(df["embedding"].values)
-        all_embeddings.append(embeddings)
+class RAGSearchClient:
+    def __init__(self, qdrant_host: str, qdrant_port: int, collection_name, embedding_model):
+        self.client = QdrantClient(host=qdrant_host, port=qdrant_port)
+        self.collection_name = collection_name
+        self.model = embedding_model
 
-        for i in range(len(embeddings)):
-            metadata.append({"file": file, "index": i})
+    def search(self, query, limit= 10, score_threshold = None, unique_by_link= True):
+        """
+        Выполняет semantic search по чанкам
 
-    all_embeddings = np.vstack(all_embeddings)
-    d = all_embeddings.shape[1]
+        :param query: текст запроса
+        :param limit: сколько чанков вернуть
+        :param score_threshold: минимальная похожесть (опционально)
+        :param unique_by_link: возвращать только 1 чанк на документ
+        """
+        query_vector = self.model.get_embedding([query])[0]
 
-    index = faiss.IndexFlatL2(d)
-    index.add(all_embeddings.astype("float32"))
+        results = self.client.query_points(
+            collection_name=self.collection_name,
+            query=query_vector.tolist(),
+            limit=limit,
+            score_threshold=score_threshold,
+            with_payload=True,
+        ).points
 
-    return index, metadata
+        output = []
+        seen_links = set()
 
-def search_nearest(query_embedding, index, metadata, k=5):
-    query = np.array(query_embedding).astype('float32').reshape(1, -1)
-    distances, indices = index.search(query, k)
-    results = []
-    for i, idx in enumerate(indices[0]):
-        results.append({**metadata[idx], 'distance': distances[0][i]})
+        for point in results:
+            payload = point.payload
+            link = payload["link"]
 
-    return results
+            if unique_by_link and link in seen_links:
+                continue
 
+            output.append({
+                "score": round(point.score, 4),
+                "text": payload["text"],
+                "link": link,
+            })
+
+            seen_links.add(link)
+
+        return output
 
 if __name__ == "__main__":
-    embedder = RuLawEmbedder("model/")
-    index, metadata = create_faiss_index("../data/processed_txt")
-
+    model = RuLawEmbedder("model/")
+    client = RAGSearchClient(qdrant_host="localhost",
+                             qdrant_port=6333,
+                             collection_name="legal_fabulas",
+                             embedding_model=model)
     query = input("Введите запрос:")
-    query_embedding = embedder.get_embedding(query)
-
-    results = search_nearest(query_embedding, index, metadata, k=10)
-    i = 1
-    print(results[-1])
-
-    # for res in results:
-    #     text_num = res["file"][4]
-    #     text_df = pd.read_parquet(f"../data/raw/text{text_num}.parquet")
-    #     index = res['index']
-    #     print(f"{i}) {text_df.iloc[index].item()}, Схожесть: {res['distance']:.2f}")
-    #     print()
-    #     i += 1
+    n = 5
+    outputs = client.search(query=query,
+                  limit=n)
+    print(outputs)
+    for i in range(len(outputs)):
+        print(f"{i + 1}) score={outputs[i]['score']} ссылка={outputs[i]['link']}")
---
+++ src/parser/parser.py
@@ -0,0 +1,139 @@
+import os
+
+import requests
+import re
+import time
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.edge.service import Service
+
+BASE_URL = "https://sudrf.cntd.ru"
+EDGE_DRIVER_PATH = "msedgedriver.exe"
+
+class Parser:
+    def __init__(self):
+        options = webdriver.EdgeOptions()
+        options.add_argument("--start-maximized")
+        # options.add_argument("--headless=new")
+        cur_filepath = os.path.dirname(__file__)
+        self.driver = webdriver.Edge(
+            service=Service(os.path.join(cur_filepath, EDGE_DRIVER_PATH)),
+            options=options
+        )
+        print("вебдрайвер загружен")
+
+    def __enter__(self):
+        return self
+
+    def close(self):
+        if self.driver:
+            self.driver.quit()
+            self.driver = None
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+    def get_links_list_page(self, links_list_url, max_scroll=0):
+        """
+        Возвращает html-код страницы со списком ссылок на документы.
+        """
+        self.driver.get(links_list_url)
+        time.sleep(3)
+
+        last_height = 0
+        same_count = 0
+
+        cnt = 0
+
+        while cnt < 120:
+            cnt += 1
+            if cnt == max_scroll:
+                break
+            self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(1.2)
+
+            new_height = self.driver.execute_script("return document.body.scrollHeight")
+            print(f"Height: {new_height}")
+
+            if new_height == last_height:
+                same_count += 1
+                if same_count >= 3:
+                    print("Достигнут конец страницы")
+                    break
+            else:
+                same_count = 0
+
+            last_height = new_height
+
+        return self.driver.page_source
+
+    def get_docs_links(self, html):
+        """
+        Сохраняет список ссылок на документы с главной страницы поиска.
+        """
+        doc_links = []
+        soup = BeautifulSoup(html, "lxml")
+        for link in soup.find_all("a", class_="document-list_i_lk"):
+            doc_links.append(BASE_URL + link.get("href"))
+        return doc_links
+
+    def parse_all(self):
+        pass
+
+
+class Chunker:
+    def get_paragraphs(self, doc_url, without_title=True):
+        """
+        Разделяет html-разметку фабулы на абзацы в чистом виде.
+        """
+        st = 2 if without_title else 0 # пропустить заголовок
+
+        keywords = ["рассмотрев материалы дела", "установил", "у с т а н о в и л", "решил", "р е ш и л"]
+        paragraphs = []
+
+        doc_html = requests.get(doc_url).text
+        soup = BeautifulSoup(doc_html, "lxml")
+        div = soup.find("div", class_="document-text_block")
+        for p in div.find_all("p", recursive=True)[st:]:
+            text = p.get_text(separator=" ", strip=True)
+
+            kw_flag = False
+            for keyword in keywords:
+                if keyword in text.lower():
+                    kw_flag = True
+                    break
+            if kw_flag:
+                break
+
+            text = re.sub(r"\s+([,.;:])", r"\1", text)
+            text = re.sub(r"\s{2,}", " ", text)
+
+            if text:
+                paragraphs.append(text + "\n")
+        return paragraphs
+
+    def chunk_paragraphs(self, paragraphs, min_chars= 200, max_chars= 1000):
+        """
+        Объединяет абзацы в чанки по размеру.
+        """
+        chunks = []
+        buffer = ""
+        for p in paragraphs:
+            if not buffer:
+                buffer = p
+                continue
+
+            if len(buffer) < min_chars:
+                buffer = buffer + " " + p
+            else:
+                chunks.append(buffer.strip())
+                buffer = p
+
+            if len(buffer) >= max_chars:
+                chunks.append(buffer.strip())
+                buffer = ""
+
+        if buffer:
+            chunks.append(buffer.strip())
+
+        return chunks
